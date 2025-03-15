69027eef9339b502578505e570dfae25c181295a
zaidanav <13522146@std.stei.itb.ac.id>
2025-03-15 13:57:20 +0700

    feat: backend foar authentication

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import templateRoutes from './routes/templateRoutes.js';
 import memberRoutes from './routes/memberRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true })); 
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use('/api', templateRoutes);
 app.use('/api', memberRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app;
