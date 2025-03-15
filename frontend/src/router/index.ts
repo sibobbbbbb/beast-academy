a83bd04585c2f14f93635d6e447adab29fc271fb
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2025-03-16 02:27:34 +0700

    fix: double router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,20 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login.vue';
-import Home from '../views/Home.vue';
-import Register from '../views/Register.vue';
-import AddMembersView from '../components/AddMembersForm.vue';
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/home', component: Home },
-  { path: '/register', component: Register },
-  {
-    path: '/add-member',
-    name: 'add-member',
-    component: AddMembersView,
-  }
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
+import Register from '../views/Register.vue'
+import AddMembersView from '../components/AddMembersForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,19 +12,9 @@ const router = createRouter({
       name: 'home',
       component: Home,
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
-    // {
-    //   path: '/template-api-fetch',
-    //   name: 'template-api-fetch',
-    //   component: () => import('../views/TemplateApiFetch.vue'),
-    // },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+    { path: '/add-member', name: 'add-member', component: AddMembersView },
     {
       path: '/userlisttest',
       name: 'userlisttest',
@@ -46,8 +24,8 @@ const router = createRouter({
       path: '/userlist',
       name: 'userlist',
       component: () => import('../views/MemberView.vue'),
-    }
+    },
   ],
 })
 
-export default router;
+export default router
