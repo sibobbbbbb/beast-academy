b9df17aaed28451831b8fb3422406b39b2692517
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2025-03-16 14:45:19 +0700

    test: ci #3

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     // Ini hanya tes untuk CI
-    console.log("Testing CI/CD Pipeline");
+    console.log("Testing CI/CD");
     console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
