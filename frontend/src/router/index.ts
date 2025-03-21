e07e4edc3d6ad15db52934038214828185f52aa7
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2025-03-21 13:56:30 +0700

    fix: error lint

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
-import Home from '../views/Home.vue'
-import Register from '../views/Register.vue'
+import Login from '../views/LoginView.vue'
+import Home from '../views/HomeView.vue'
+import Register from '../views/RegisterView.vue'
 import AddMembersView from '../components/AddMembersForm.vue'
 
 const router = createRouter({
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -119,7 +119,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import api from '@/utils/axios';
