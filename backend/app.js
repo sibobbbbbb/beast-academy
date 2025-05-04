3be9be3529b9d21a7790582bddeba134aff82658
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2025-05-04 22:56:53 +0700

    feat: update cors config

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,20 @@ import noteRoutes from './routes/noteRoutes.js';
 
 const app = express();
 
-// Middleware
-// app.use(cors({ origin: 'http://localhost:5173', credentials: true })); 
-app.use(cors({
-    origin: true, // reflect request origin
+// Middlewares
+if (process.env.NODE_ENV === 'production') {
+  app.use(cors({
+    origin: process.env.FRONTEND_URL,
     credentials: true
   }));
-console.warn("MNKi | DEV MODE, CORS SET TO TRUE, DO NOT FORGET TO CHANGE ON DEPLOY")
+} else {
+  app.use(cors({
+      origin: true,
+      credentials: true
+    }));
+  console.warn("MNKi | DEV MODE, CORS SET TO TRUE, DO NOT FORGET TO CHANGE ON DEPLOY")
+}
+
 app.use(express.json());
 app.use(cookieParser());
 
