edc4ff671d811872c9df403c2230fbcad9b32ed4
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2025-05-06 09:04:55 +0700

    feat: test ci cd

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     // Ini hanya tes untuk CI/CD
-    console.log("Testing CI/CD pipelines JOBS on Gitlab test again 7");
+    console.log("Testing CI/CD pipelines JOBS on Gitlab test again 8");
     console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
