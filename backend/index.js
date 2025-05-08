31cb46383779d543bff78121655db43323f0ceee
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2025-05-08 15:50:06 +0700

    feat: deploy setup

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,5 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    // Ini hanya tes untuk CI/CD
-    console.log("Testing CI/CD pipelines JOBS on Gitlab test again 8");
-    console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
+    console.log(`🚀 Server berjalan di ${PORT}`);
 });
